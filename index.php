6992ea1ca9915ee2efb790e2727beee8503a013f
youssouf doucoure <madkdouc@yahoo.com>
2024-03-19 05:04:32 +0100

    index php

--- index.php
+++ index.php
@@ -1,3 +1,121 @@
+<?php
+
+$firstname = $name = $email = $phone = $message = '';   // les variable du formulaire
+
+$firstnameError = $nameErroR = $emailError = $phoneError = $messageError = ''; // variable erreur dans le formulaire
+
+
+// toute cette condition sera exécuter une fois que le client rentrera ces données 
+
+if ($_SERVER["REQUEST_METHOD"] == "POST")           // condition requete method egale post
+{
+    $firstname = verifyInput($_POST["firstname"]);
+    $name = verifyInput($_POST["name"]);
+    $email = verifyInput($_POST["email"]);
+    $phone = verifyInput($_POST["phone"]);
+    $message = verifyInput($_POST["message"]);
+    $isSuccess = true ;
+    $emailText = "" ;     // le mail a qui sera envoyé
+
+
+
+    if(empty($firstname))           // cette condition sert a quand utilisateur ne rentre pas ces donner lui affichera ce message
+    {
+        $firstnameError = "Je veux connaitre ton prénom";
+        $isSuccess = false ;
+    }
+    else 
+    {
+        $emailText .= "firstname : $firstname\n";
+    }
+
+
+
+    if(empty($name))
+    {
+        $nameErroR = " Et oui je veux tout savoir. Mème ton nom !!";
+        $isSuccess = false ;
+    }
+    else{
+        $emailText .= "name : $name\n";
+    }
+
+
+
+    if(empty($message))
+    {
+        $messageErroR = " Et oui je veux tout savoir !!";
+        $isSuccess = false ;
+    }
+    else
+    {
+        $emailText .= "message : $message\n";
+    }
+
+
+
+    if(!isEmail($email))  //  si message d'erreur... il sagit de la validation du email
+    {
+        $emailError = "T'essaies de me rouler ? C'est pas un email !!";
+        $isSuccess = false ;
+    }
+    else
+    {
+        $emailText .= "email : $email\n";
+    }
+
+
+
+    if(!isPhone($phone))   // si message d'erreur , sa affiche message .. 
+    {
+        $phoneError = "Que des chiffres et des espaces, stp ...";
+        $isSuccess = false ;
+    }
+    else
+    {
+        $emailText .= "Phone : $phone\n";
+    }
+
+
+
+    if($isSuccess)
+    {
+        //  LA FONCTION qui envoi le contenue de l'email 
+
+        $headers = "From: $firstname $name <$email>\r\nReply-To: $email";
+        mail($emailTo, "un message de votre site", $emailText , $headers);
+        $firstname = $name = $email = $phone = $message = ''; // les champ des variable seront vide avec cette fonction 
+    }
+
+
+}
+
+// fonction qui relie les condition 
+
+function isPhone($var)
+{
+    return preg_match("/^[0-9 ]*$/", $var);   // expression reguliere pour le nombre de caractère pour le phone
+}
+
+
+function isEmail($var)
+{
+    return filter_var($var, FILTER_VALIDATE_EMAIL);
+}
+
+
+function verifyInput ($var)         // element pour masquer les element html dans la fonction
+{
+    $var = trim($var);
+    $var = stripslashes($var);
+    $var = htmlspecialchars($var);
+
+    return $var ;
+}
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -20,36 +138,36 @@
     
         <div class="row">
             <div class="col-lg-10 col-lg-offset-1">
-                <form id="contact-form" method="post" action="" role="form">
+                <form id="contact-form" method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" role="form">
                     <div class="row">
                         <div class="col-md-6">
                             <label for="firstname">Prénom<span class="bleu">*</span></label>
-                            <input type="text" id="fistname" name="firstname" class="form-control" placeholder="Votre prenom">
-                            <p class="comments">Message d'erreur</p>
+                            <input type="text" id="fistname" name="firstname" required class="form-control" placeholder="Votre prenom" value="<?php echo $firstname ;?>">
+                            <p class="comments"><?php echo $firstnameError;?></p>
                         </div>
 
                         <div class="col-md-6">
                             <label for="name">Nom<span class="bleu">*</span></label>
-                            <input type="text" id="name" name="name" class="form-control" placeholder="Votre prenom">
-                            <p class="comments">Message d'erreur</p>
+                            <input type="text" id="name" name="name" class="form-control" placeholder="Votre nom" value="<?php echo $name ;?>">
+                            <p class="comments"><?php echo $nameError;?></p>
                         </div>
 
                         <div class="col-md-6">
                             <label for="email">Email<span class="bleu">*</span></label>
-                            <input type="text" id="email" name="email" class="form-control" placeholder="Votre téléphone">
-                            <p class="comments">Message d'erreur</p>
+                            <input type="text" id="email" required name="email" class="form-control" placeholder="Votre email" value="<?php echo $email ;?>">
+                            <p class="comments"><?php echo $emailError ;?></p>
                         </div>
 
                         <div class="col-md-6">
                             <label for="name">Téléphone</label>
-                            <input type="text" id="phone" name="phone" class="form-control" placeholder="Votre prenom">
-                            <p class="comments">Message d'erreur</p>
+                            <input type="text" id="phone" name="phone" class="form-control" placeholder="Votre telephone" value=" <?php echo $phone ;?>">
+                            <p class="comments"><?php echo $phoneError;?></p>
                         </div>
 
                         <div class="col-md-12">
                             <label for="message">Message<span class="bleu"> *</span></label>
-                            <textarea id="message" name="message" class="form-control" placeholder="Votre message" rows="4" ></textarea>
-                            <p class="comments">Message d'erreur</p>
+                            <textarea id="message" name="message" class="form-control" placeholder="Votre message" rows="4" ><?php echo $message ;?></textarea>
+                            <p class="comments"><?php echo $messageError;?></p>
                         </div>
 
                         <div class="col-md-12">
@@ -62,8 +180,8 @@
 
 
                     
-
-                            <p class="thank-you">Votre message a bien ete envoye. Merci de m'avoir contacte :) </p>
+                                <!---- le message apparaitra une fois que le formulaire a été validé avec ( succes)--->
+                            <p class="thank-you" style="display:<?php if($isSuccess) echo 'block'; else echo 'none' ;?> " >Votre message a bien ete envoye. Merci de m'avoir contacte :) </p>
                     </form>
                 </div>
             </div>
